5a99263fdbd53dc18e6f6ed019a88a76dca0620a
Roger <rscheffeld@gmail.com>
2020-08-31 17:10:23 -0300

    Alterando a API de consulta de CEP.

--- src/services/api.js
+++ src/services/api.js
@@ -1,11 +1,7 @@
 const axios = require('axios').default;
 
 const api = axios.create({
-    baseURL: 'https://webmaniabr.com/api/1/cep/',
-    params: {
-        app_key: 'Yz1vL5BueMLid1xeHwVKRzZGd2Jj2JOU',
-        app_secret: 'LzoB9kl6JwyFWlqMPUZOH0KQU2x2igYXJpen2KalqCcnTIWv'
-    }
+    baseURL: 'https://api.postmon.com.br/v1/cep/',
 })
 
 module.exports = api
