96d465329d5750a1808642b162f0fdd45a8d7b39
Roger <rscheffeld@gmail.com>
2020-09-02 11:38:13 -0300

    Adicionando documentação com Swagger.

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const app = express();
 const routes = require('./src/routes/routes')
+const swaggerUi = require('swagger-ui-express');
+const swaggerDoc = require('./swagger.json');
 
 /**
  * Adicionando Middlewares
  */
 app.use(express.json());
-app.use(routes)
+app.use(routes);
+app.use('/api-docs/v1', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 
 
 module.exports = app
