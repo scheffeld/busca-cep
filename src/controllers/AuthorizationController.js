d1f5f0ea27ce966b47548443e3f16dc42748d6ec
Roger <rscheffeld@gmail.com>
2020-09-03 22:35:05 -0300

    Criando controller de autorização.

---
+++ src/controllers/AuthorizationController.js
@@ -0,0 +1,14 @@
+const jwt = require('../util/jwt')
+
+module.exports = {
+    async generateToken(req, res){
+        const userInfo = {
+            user: 'admin',
+            pass: 'admin'
+        }
+    
+        const token = jwt.sign(userInfo);
+    
+        return res.json({ token })
+    }
+}
