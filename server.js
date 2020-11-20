7a9c3ded9ff555aafbd23010b37d49a1442fd36f
Roger <rscheffeld@gmail.com>
2020-11-19 23:47:32 -0300

    add(server): configure project to deploy
    - add port env

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const app = require('./app');
 
-/**
- * O servidor da aplicação, será executado na porta 3333 do localhost
- */
-app.listen(3333);
+const port = process.env.PORT || 3333
+
+app.listen(port);
